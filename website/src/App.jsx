406d602b9335bcead11fc861b6052449aa70996a
anupsb2003 <anupsb2003@gmail.com>
2026-03-07 01:55:43 +0530

    add weather

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,9 +1,11 @@
 import Home from "./pages/Home";
-
+import WeatherDashboard from "./pages/WeatherDashboard";
 function App() {
   return (
     <div>
+      
       <Home />
+      {/* <WeatherDashboard /> */}
     </div>
   );
 }
